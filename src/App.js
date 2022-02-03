24a4d7f2911d112b0f36316d3b916d360608675f
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-02-03 18:08:12 -0500

    routes, tailwind & daisyiu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar'
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -8,8 +12,17 @@ function App() {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
 
-        <main>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
+
     </Router>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <div class="mockup-window bg-base-300">
+        <div class="flex justify-center px-4 py-16 bg-base-200 text-6xl">
+          Welcome
+        </div>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default Home
